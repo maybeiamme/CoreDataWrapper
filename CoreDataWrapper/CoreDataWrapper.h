5eacbecb14337d4f44695e3b1738a925390b609a
1002220 <jin.hyoung.park@sk.com>
2016-12-01 11:41:56 +0900

    initial commit

---
+++ CoreDataWrapper/CoreDataWrapper.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataWrapper.h
+//  CoreDataWrapper
+//
+//  Created by 1002220 on 2016. 12. 1..
+//  Copyright © 2016년 n29. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataWrapper.
+FOUNDATION_EXPORT double CoreDataWrapperVersionNumber;
+
+//! Project version string for CoreDataWrapper.
+FOUNDATION_EXPORT const unsigned char CoreDataWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataWrapper/PublicHeader.h>
+
+
